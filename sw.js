99d88bc2c014d175b71d942a6fc393d85e7e54cf
Nani2554 <formeextraformeextra@gmail.com>
2026-01-18 13:31:07 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,18 +1,46 @@
-const CACHE_NAME = "futureplay-cache-v1";
-const FILES_TO_CACHE = [
-  "/",
-  "/index.html",
-  "/manifest.json"
-];
+const puzzle = document.getElementById("puzzle");
+const message = document.getElementById("message");
 
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
-  );
-});
+let tiles = [1, 2, 3, 4, 5, 6, 7, 8, ""];
 
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(res => res || fetch(event.request))
-  );
-});
+function createPuzzle() {
+  puzzle.innerHTML = "";
+  tiles.forEach((tile, index) => {
+    const div = document.createElement("div");
+    div.className = tile === "" ? "tile empty" : "tile";
+    div.innerText = tile;
+    div.addEventListener("click", () => moveTile(index));
+    puzzle.appendChild(div);
+  });
+}
+
+function moveTile(index) {
+  const emptyIndex = tiles.indexOf("");
+  const validMoves = [
+    index - 1,
+    index + 1,
+    index - 3,
+    index + 3
+  ];
+
+  if (validMoves.includes(emptyIndex)) {
+    [tiles[index], tiles[emptyIndex]] = [tiles[emptyIndex], tiles[index]];
+    createPuzzle();
+    checkWin();
+  }
+}
+
+function shufflePuzzle() {
+  tiles.sort(() => Math.random() - 0.5);
+  message.innerText = "";
+  createPuzzle();
+}
+
+function checkWin() {
+  const win = [1, 2, 3, 4, 5, 6, 7, 8, ""];
+  if (JSON.stringify(tiles) === JSON.stringify(win)) {
+    message.innerText = "🎉 Congratulations! You solved the puzzle!";
+  }
+}
+
+createPuzzle();
