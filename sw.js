c63f2863e5d9c94be41e52cafb1a9c9d6570806a
Nani2554 <formeextraformeextra@gmail.com>
2026-01-18 13:44:29 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,46 +1,44 @@
-const puzzle = document.getElementById("puzzle");
-const message = document.getElementById("message");
+document.addEventListener("DOMContentLoaded", () => {
+  const puzzle = document.getElementById("puzzle");
+  const message = document.getElementById("message");
 
-let tiles = [1, 2, 3, 4, 5, 6, 7, 8, ""];
+  let tiles = [1, 2, 3, 4, 5, 6, 7, 8, ""];
 
-function createPuzzle() {
-  puzzle.innerHTML = "";
-  tiles.forEach((tile, index) => {
-    const div = document.createElement("div");
-    div.className = tile === "" ? "tile empty" : "tile";
-    div.innerText = tile;
-    div.addEventListener("click", () => moveTile(index));
-    puzzle.appendChild(div);
-  });
-}
+  function createPuzzle() {
+    puzzle.innerHTML = "";
+    tiles.forEach((tile, index) => {
+      const div = document.createElement("div");
+      div.className = tile === "" ? "tile empty" : "tile";
+      div.innerText = tile;
+      div.addEventListener("click", () => moveTile(index));
+      puzzle.appendChild(div);
+    });
+  }
 
-function moveTile(index) {
-  const emptyIndex = tiles.indexOf("");
-  const validMoves = [
-    index - 1,
-    index + 1,
-    index - 3,
-    index + 3
-  ];
+  function moveTile(index) {
+    const emptyIndex = tiles.indexOf("");
+    const validMoves = [index - 1, index + 1, index - 3, index + 3];
 
-  if (validMoves.includes(emptyIndex)) {
-    [tiles[index], tiles[emptyIndex]] = [tiles[emptyIndex], tiles[index]];
+    if (validMoves.includes(emptyIndex)) {
+      [tiles[index], tiles[emptyIndex]] = [tiles[emptyIndex], tiles[index]];
+      createPuzzle();
+      checkWin();
+    }
+  }
+
+  function shufflePuzzle() {
+    tiles.sort(() => Math.random() - 0.5);
+    message.innerText = "";
     createPuzzle();
-    checkWin();
   }
-}
 
-function shufflePuzzle() {
-  tiles.sort(() => Math.random() - 0.5);
-  message.innerText = "";
+  function checkWin() {
+    const win = [1, 2, 3, 4, 5, 6, 7, 8, ""];
+    if (JSON.stringify(tiles) === JSON.stringify(win)) {
+      message.innerText = "🎉 Congratulations! You solved the puzzle!";
+    }
+  }
+
+  window.shufflePuzzle = shufflePuzzle;
   createPuzzle();
-}
-
-function checkWin() {
-  const win = [1, 2, 3, 4, 5, 6, 7, 8, ""];
-  if (JSON.stringify(tiles) === JSON.stringify(win)) {
-    message.innerText = "🎉 Congratulations! You solved the puzzle!";
-  }
-}
-
-createPuzzle();
+});
